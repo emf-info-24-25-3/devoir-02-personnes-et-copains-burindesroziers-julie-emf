278b4feac680747ffc75c9e27e761646e43d50d4
burindesroziers-julie-emf <julie.dosreis@studentfr.ch>
2025-02-11 13:51:35 +0100

    corrigé

--- src/models/Personne.java
+++ src/models/Personne.java
@@ -5,11 +5,12 @@ public class Personne {
         private final String nom;
         private final String prenom;
         private static final int MAX_COPAINS = 10;
-        private Personne[] copains = new Personne[MAX_COPAINS];
+        private Personne[] copains;
 
         public Personne(String nom, String prenom) {
                 this.nom = nom;
                 this.prenom = prenom;
+                this.copains  = new Personne[MAX_COPAINS];
         }
 
         public String getNom(){
@@ -27,6 +28,9 @@ public class Personne {
                                 aDesCopains = true;
                         }
                 }
+                if (aDesCopains==false) {
+                        formulation += ")";
+                }
                 Personne dernierCopain = null;
                 for (int i = copains.length-1; i >=0; i--) {
                         if (copains[i]!=null) {
@@ -37,9 +41,6 @@ public class Personne {
                 for (int i = 0; i < copains.length; i++) {
                         if (copains[i]!=null && copains[i]!=dernierCopain) {
                                 formulation +=copains[i].getPrenom()+" "+copains[i].getNom()+", ";
-                        }else if(aDesCopains==false){
-                                formulation += ")";
-                                break;
                         }else if (copains[i]==dernierCopain) {
                                 formulation += copains[i].getPrenom()+" "+copains[i].getNom()+")";
                         }
@@ -47,28 +48,37 @@ public class Personne {
                 return formulation;
         }
 
-        public void tenterAjoutCopain(Personne copain) {
+        public boolean tenterAjoutCopain(Personne copain) {
+                boolean aReussi=true;
                 boolean estPresent = false;
+                int nbreCopains = 0;
                 for (int i = 0; i < copains.length; i++) {
+                        if (copains[i]!=null) {
+                                nbreCopains++;
+                        }
                         if (copains[i] == copain) {
                                 estPresent = true;
                         }
                 }
+                if (estPresent) {
+                        aReussi=false;
+                        System.out.println(copain.getPrenom()+" "+copain.getNom()+" est déjà dans la liste");
+                }
                 for (int i = 0; i < copains.length; i++) {
-                        if (copains[i] == null & estPresent==false) {
+                        if (copains[i] == null) {
                                 copains[i] = copain;
                                 System.out.println("Ajout de l'ami : " + copain.getPrenom()+" "+copain.getNom());
                                 break;
-                        }else if(estPresent==true){
-                                System.out.println(copain.getPrenom()+" "+copain.getNom()+" est déjà dans la liste");
-                                break;
-                        }else if(copains[MAX_COPAINS-1] != null & estPresent==false){
+                        }else if(nbreCopains==10){
+                                aReussi=false;
                                 System.out.println("Trop d'ami : échec de l'ajout");
                         }
                 }
+                return aReussi;
         }
 
-        public void tenterSupprimerCopain(Personne copain) {
+        public boolean tenterSupprimerCopain(Personne copain) {
+                boolean aReussi=true;
                 boolean estPresent = false;
                 boolean aDesCopains = false;
                 for (int i = 0; i < copains.length; i++) {
@@ -80,17 +90,21 @@ public class Personne {
                                 aDesCopains = true;
                         }
                 }
+                if (estPresent==false) {
+                        aReussi =false;
+                        System.out.println(copain.getPrenom()+" "+copain.getNom()+" n'est pas dans la liste");
+                }
+                if (aDesCopains==false) {
+                        aReussi=false;
+                        System.out.println("Pas d'ami : échec de suppression");
+                }
                 for (int i = 0; i < copains.length; i++) {
                         if (copains[i] == copain & estPresent==true) {
                                 copains[i] = null;
                                 System.out.println("Suppression de l'ami : " + copain.getPrenom()+" "+copain.getNom());
                                 break;
-                        }else if(estPresent==false){
-                                System.out.println(copain.getPrenom()+" "+copain.getNom()+" n'est pas dans la liste");
-                                break;
-                        }else if(copains[MAX_COPAINS-1] == null & aDesCopains==false){
-                                System.out.println("Pas d'ami : échec de suppression");
                         }
                 }
+                return aReussi;
         }
 }
